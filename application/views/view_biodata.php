fec45b60d00c6b1c4df2c34564972019658202ea
FallenReza <gamesreza10@gmail.com>
2023-12-12 15:08:28 +0700

    first commit

---
+++ application/views/view_biodata.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+</head>
+<style>
+    *{
+        font-family: 'Poppins';
+        color: white;
+    }
+    body{
+        background: rgb(131,58,180);
+        background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
+    }
+</style>
+<body>
+
+
+<h1 align="center">Biodata Saya</h1>
+
+    <table align="center" font-size="50px">
+        <pre>
+        <tr>
+            <td>Nama :</td>
+            <td>Reza Rabbani Rachim</td>
+        </tr>
+        <tr>
+            <td>Alamat : </td>
+            <td>Jl. Flamboyan 7 Menteng Dalam</td>
+        </tr>
+        </pre>
+        
+        <tr>
+            <td>Jurusan : </td>
+            <td>Rekayasa Perangkat Lunak</td>
+        </tr>
+        <tr>
+        <td>Alamat : </td>
+            <td>Jl. Flamboyan 7 Menteng Dalam</td>
+        </tr>
+        <tr>
+        <td>Kelas : </td>
+            <td>10.3A.07</td>
+        </tr>
+        <tr>
+        <td>No.HP : </td>
+            <td>089668788392</td>
+        </tr>
+    </table>
+
+</body>
+</html>
